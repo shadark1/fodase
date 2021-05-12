9b43a54c8346f7c1ab61b247e02ce659edbb994a
shadark1 <hickijunqueira@hotmail.com>
2021-05-12 11:05:34 -0300

    Atualização 0.0.2, correções de grafia, criação de botão em telas até 454px, correção de script de envio de formulário

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+//1 – Definimos Para quem vai ser enviado o email
+$para = "contato@onixrefrigeracao.com.br";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$nome = $_POST['name'];
+$email = $_POST['email'];
+$mensagem = $_POST['message']
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel
+//$assunto
+$assunto = $_POST['subject'];
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$mensagem = "<strong>Nome:  </strong>".$nome . "<strong>Email: </strong>" . $email;
+$mensagem .= "<br>  <strong>Mensagem: </strong>"
+.$_POST['mensagem'];
+
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  Formulario Site <contato@onixrefrigeracao.com.br>\n";
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <contato@onixrefrigeracao.com.br>\n";
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <contato@onixrefrigeracao.com.br>\n";
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
+
+mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
+
+echo "<script> alert('Mensagem enviada!') </script>"
+?>
